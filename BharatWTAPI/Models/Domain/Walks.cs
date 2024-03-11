d70ce26c347f04635b2cb06bc455b1a03edccbb2
Shivam Dongre <shivamdongreji62@gmail.com>
2024-03-11 23:58:56 +0530

    Commit First

---
+++ BharatWTAPI/Models/Domain/Walks.cs
@@ -0,0 +1,11 @@
+﻿namespace BharatWTAPI.Models.Domain
+{
+    public class Walks
+    {
+        public Guid Id { get; set; }
+        public string  Name { get; set; }
+        public string  Description { get; set; }
+        public double  Length { get; set; }
+        public string? WalkImageUrl { get;set; }    
+    }
+}
