14f19ef0a95303b734ba597d863e1e8e31dfb0cb
Shivam Dongre <shivamdongreji62@gmail.com>
2024-03-12 00:09:42 +0530

    Commit All Domain Changes Done

--- BharatWTAPI/Models/Domain/Walks.cs
+++ BharatWTAPI/Models/Domain/Walks.cs
@@ -7,5 +7,12 @@
         public string  Description { get; set; }
         public double  Length { get; set; }
         public string? WalkImageUrl { get;set; }    
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+
+        //Simple Navigation Property
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }      
     }
 }
