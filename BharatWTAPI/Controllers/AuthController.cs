2fddd4ffe3f7ea6153739add25ca05c088f4c905
Shivam Dongre <shivamdongreji62@gmail.com>
2024-03-25 16:12:13 +0530

    Commit For Auth Controller

---
+++ BharatWTAPI/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using BharatWTAPI.Models.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BharatWTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        [Route("register")]
+        public async Task<IActionResult> Register(RegisterRequestDTO model)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = model.UserName,
+                Email = model.UserName
+            };
+            var identityResult = await userManager.CreateAsync(identityUser);
+
+            if (identityResult.Succeeded)
+            {
+                //Adding Roles Here
+                if (model.Roles != null)
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, model.Roles);
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User Registered");
+                    }
+
+                }
+
+            }
+
+            return BadRequest("Error");
+        }
+    }
+}
---
+++ BharatWTAPI/Models/DTO/RegisterRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BharatWTAPI.Models.DTO
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        public string[] Roles { get; set; } 
+
+    }
+}
